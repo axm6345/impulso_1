e45d878a2eda2389e07ba77922a4c0bc4eb26ee2
ahm10 <atharvamungee@gmail.com>
2020-09-28 17:41:40 -0400

    initial commit

--- Medium_scrape_text_and_image/2009-2016/article_text_img_scraping_updated.py
+++ Medium_scrape_text_and_image/2009-2016/article_text_img_scraping_updated.py
@@ -10,7 +10,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 
-import shortuuid 
+import shortuuid
 import json
 
 
@@ -35,43 +35,43 @@ def filter_string(a_string):
         return False
     else:
         return True
-    
+
 def get_uuid(url):
 
     # GENERATE A UNIQUE ID BASED ON THE URL
-    
+
     unique_id = shortuuid.uuid(name=url)
-      
+
     return unique_id
 
 class medium_article_scrapping():
     """
     CLASS DEFINITION TO SCRAPE ALL DATA FOR AN ARTICLE AND RETURN JSON OUTPUT.
     """
-    
+
     def __init__(self):
-        self.scraper_class = 'medium_article_scrapping' 
-        
-     
-      
+        self.scraper_class = 'medium_article_scrapping'
+
+
+
     def get_title(self, soup):
         try:
             title = soup.find('h1').text
             return title
-        
+
         except:
             print("Couldn't get title from article.")
-        
+
     def get_subtitle(self, soup):
         try:
             subtitle_soup = soup.find('h1').parent.parent.next_sibling.find('h2')
             subtitle = clean_string(subtitle_soup.text)
             return subtitle
-        
+
         except:
-            print("Couldn't (or didn't) get subtitle from article.") 
+            print("Couldn't (or didn't) get subtitle from article.")
             return 'None'
-            
+
     def get_tags(self, soup):
         try:
             tags = []
@@ -80,7 +80,7 @@ class medium_article_scrapping():
                 return 'None'
             else:
                 return tags
-            
+
         except:
             print("Couldn't get article tags.")
 
@@ -94,22 +94,22 @@ class medium_article_scrapping():
                 return 'None'
             else:
                 return tags_link
-            
+
         except:
-            print("Couldn't get article tag links.")           
-    
+            print("Couldn't get article tag links.")
+
     def get_author(self, soup):
         try:
             author = soup.find('div',style=re.compile(r'flex:.*')).find('a').text
             return author
         except:
             print("Couldn't get author from article.")
-            
+
     def get_h1_headers(self, soup):
         try:
             article_soup = soup.find('article')
             h1_header_soups = article_soup.find_all('h1')
-            
+
             if len(h1_header_soups) == 1:
                 return 'None'
             else:
@@ -117,29 +117,29 @@ class medium_article_scrapping():
                 return h1_headers
         except:
             print("Couldn't get h1 headers.")
-            
-    
+
+
     def get_h2_headers(self, soup):
         try:
             h2_headers = []
-            
+
             article_soup = soup.find('article')
             h2_header_soups = article_soup.find_all('h2')
-            
+
             for header_soup in h2_header_soups:
                 if header_soup.text == "Dive in. We'll learn what you like along the way.":
                     continue
                 else:
                     h2_headers.append(clean_string(header_soup.text))
-            
+
             if len(h2_headers) == 0:
                 return 'None'
             else:
                 return h2_headers
-        
+
         except:
             print("Couldn't get h2 headers.")
-            
+
     def get_paragraphs(self, soup):
         try:
             article_soup = soup.find('article')
@@ -147,7 +147,7 @@ class medium_article_scrapping():
             return paragraphs
         except:
             print("Couldn't scrape paragraphs.")
-            
+
     def get_blockquotes(self, soup):
         try:
             article_soup = soup.find('article')
@@ -156,7 +156,7 @@ class medium_article_scrapping():
         except:
             print("Couldn't (or didn't) find blockquotes.")
             return 'None'
-            
+
     def get_bolded(self,soup):
         try:
             article_soup = soup.find('article')
@@ -164,43 +164,43 @@ class medium_article_scrapping():
             bolded = [x.text.strip() for x in article_soup.find_all('strong')]
             bolded = [clean_string(x) for x in bolded if filter_string(x)]
             bolded = [x for x in bolded if x]
-          
+
             if len(bolded) == 0:
                 return 'None'
             else:
                 return bolded
         except:
             print("Couldn't get bolded text.")
-    
+
     def get_italics(self, soup):
         try:
             article_soup = soup.find('article')
             italics = [x.text.strip() for x in article_soup.find_all('em')]
             italics = [clean_string(x) for x in italics if filter_string(x)]
-            
+
             if len(italics) == 0:
                 return 'None'
             else:
                 return italics
         except:
             print("Couldnt get italicized text.")
-            
+
     def count_bullet_lists(self, soup):
         try:
             article_soup = soup.find('article')
             return len(article_soup.find_all('ul'))
-        
+
         except:
             print("Couldn't count bullet lists.")
-            
+
     def count_numbered_lists(self, soup):
         try:
             article_soup = soup.find('article')
             return len(article_soup.find_all('ol'))
-        
+
         except:
             print("Couldn't count numbered lists.")
-            
+
     def figures(self, soup):
         try:
             article_soup = soup.find('article')
@@ -212,10 +212,10 @@ class medium_article_scrapping():
                 img_replace = img_split.replace('60','1024')
                 img_info.append((img_replace, img_tag[0]["alt"]))
             return img_info
-        
+
         except:
-            print("Couldn't find images.")  
-            
+            print("Couldn't find images.")
+
     def count_gists(self, soup):
         try:
             gists = []
@@ -226,21 +226,21 @@ class medium_article_scrapping():
                     continue
                 else:
                     gists.append(gist_soup)
-                    
+
             return len(gists)
-        
+
         except:
             print("Couldn't count gists.")
-            
+
     def count_code_chunks(self, soup):
         try:
             article_soup = soup.find('article')
             code_chunk_soups = article_soup.find_all('pre')
             return len(code_chunk_soups)
-        
+
         except:
             print("Couldn't count code chunks.")
-            
+
     def count_vids(self, soup):
         try:
             yt_vids = []
@@ -251,12 +251,12 @@ class medium_article_scrapping():
                     continue
                 else:
                     yt_vids.append(yt_soup)
-                    
+
             return len(yt_vids)
-                    
+
         except:
-            print("Couldn't get YouTube videos.")        
-            
+            print("Couldn't get YouTube videos.")
+
     def links(self, soup):
         try:
             article_soup = soup.find('article')
@@ -265,9 +265,9 @@ class medium_article_scrapping():
             for link in link_soups:
                 links.append(link.get('href'))
             return links
-        
+
         except:
-            print("Couldn't find links.") 
+            print("Couldn't find links.")
 
     def ext_links(self, soup):
         try:
@@ -277,13 +277,13 @@ class medium_article_scrapping():
             for ext_link in ext_link_soups:
                 ext_links.append(ext_link.get('href'))
             return ext_links
-        
+
         except:
-            print("Couldn't find external links.")      
-            
+            print("Couldn't find external links.")
+
     def scrape(self, url,soup):
         article_data = {
-            
+
             "UUID" : get_uuid(url),
             "url" : url,
             "title": self.get_title(soup),
@@ -307,27 +307,27 @@ class medium_article_scrapping():
            # "n_vids": self.count_vids(soup),
            # "n_links": self.count_links(soup),
         }
-        
+
         # if subtitle exists, remove it from h2_headers list
         subtitle = article_data['subtitle']
         if subtitle != 'None':
             article_data['h2_headers'].remove(subtitle)
-            
+
             if len(article_data['h2_headers']) == 0:
                 article_data['h2_headers'] = 'None'
-                
+
         return(article_data)
 
 # LOAD CSV AND FETCH ARTICLE URLS TO BE SCRAPPED
-url_file_name = "Medium_scrape_urls_multi-tag _clean_2016-2020"
+url_file_name = "Medium_scrape_urls_multi-tag _clean_2009-2016"
 url_file = url_file_name + ".csv"
 urls_df = pd.read_csv(url_file)
 # urls_df.head()
 # N = no of articles to be scrapped
-N = 5
+N = 4619
 #N = len(urls_df)
 # MAKE LISTS
-urls = urls_df.url.to_list() 
+urls = urls_df.url.to_list()
 #topics = urls_df.Tag.to_list()
 #scrape_urls = urls[:N]
 
@@ -347,14 +347,14 @@ content = medium_article_scrapping()
 # driver.close()
 
 # SCRAPE FOR MULTIPLE/ALL URLS
-content_df = pd.DataFrame(columns = ['UUID', 'url','title', 'subtitle','tags','tags_link', 'author', 'h1_headers', 'h2_headers', 'paragraphs', 
-                                     'blockquotes', 'bold_text', 'italic_text', 
+content_df = pd.DataFrame(columns = ['UUID', 'url','title', 'subtitle','tags','tags_link', 'author', 'h1_headers', 'h2_headers', 'paragraphs',
+                                     'blockquotes', 'bold_text', 'italic_text',
                                      'figures', 'links', 'external_links'
                                      #'n_bullet_lists',
-                                     #'n_numbered_lists', 
-                                     #'n_gists', 
-                                     #'n_code_chunks', 
-                                     #'n_vids', 
+                                     #'n_numbered_lists',
+                                     #'n_gists',
+                                     #'n_code_chunks',
+                                     #'n_vids',
                                      #'n_links'
                                      ])
 
@@ -371,7 +371,7 @@ options.add_argument("--disable-dev-shm-usage")
 
 start = datetime.now()
 
-for article_i in range(N):   
+for article_i in range(N):
 
     url = urls[article_i]
     #topic = topics[article_i]
@@ -384,13 +384,13 @@ for article_i in range(N):
     except Exception:
         print ('article not found')
 
-    
+
     time.sleep(1)
     soup = BeautifulSoup(driver.page_source, 'lxml')
 
     data_scrape = content.scrape(url, soup)
     content_df = content_df.append(data_scrape, ignore_index=True,  sort=False)
-    
+
     soup.decompose()
 
     driver.close()
@@ -403,13 +403,13 @@ print('Scraping done!')
 output_file = 'contents_op_' + url_file
 content_df.to_csv(output_file, index=False)
 
-# Convert to JSON 
+# Convert to JSON
 json_file = url_file_name + ".json"
 print("writing into..",json_file)
 content_df.to_json(json_file,orient="records",force_ascii=False)
 print("Completed!")
 #parsed_content = json.loads(content_df_json)
-#op_json = json.dumps(parsed_content,indent=4)    
+#op_json = json.dumps(parsed_content,indent=4)
 #print("JSON....")
 
 #print(op_json)
